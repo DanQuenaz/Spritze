4c3f49ddee2337cadd07bb0009be35ec892e4516
Dan Pimentel <danquenazz@gmail.com>
2017-11-09 22:05:34 -0200

    bug

--- app/src/main/java/com/unifei/spritze/spritze/dosageActivity.java
+++ app/src/main/java/com/unifei/spritze/spritze/dosageActivity.java
@@ -1,24 +1,22 @@
 package com.unifei.spritze.spritze;
 
-<<<<<<< HEAD
+
 import android.app.Activity;
-=======
+
 import android.content.Intent;
->>>>>>> 3c92980feb2e44cef5520d9890b992ec96bd17e5
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-<<<<<<< HEAD
-public class dosageActivity extends Activity {
-=======
+
 public class dosageActivity extends AppCompatActivity {
     private String pacientName;
     private Long pacientAge;
     private double pacientWeight;
     private TextView pctName;
     private TextView pctAge;
->>>>>>> 3c92980feb2e44cef5520d9890b992ec96bd17e5
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
