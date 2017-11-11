60296473054f5ed7bf5abc7ceaaf182c0a087c58
Guilherme <guioalim@gmail.com>
2017-11-10 22:48:09 -0200

    Mudanca de mais telas pro material e adicionar search na tela de hospitais

--- app/src/main/java/com/unifei/spritze/spritze/MainActivity.java
+++ app/src/main/java/com/unifei/spritze/spritze/MainActivity.java
@@ -1,5 +1,6 @@
 package com.unifei.spritze.spritze;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Point;
--- app/src/main/java/com/unifei/spritze/spritze/hospital_select.java
+++ app/src/main/java/com/unifei/spritze/spritze/hospital_select.java
@@ -1,14 +1,21 @@
 package com.unifei.spritze.spritze;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
@@ -17,36 +24,39 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import entities.Pacient;
 import firebase.ConfigFireBase;
 
-public class hospital_select extends Activity {
-    private DatabaseReference rootDB;
+public class hospital_select extends Activity implements SearchView.OnQueryTextListener{
+    private DatabaseReference rootBD;
     private ListView hospList;
     private String medicEmail;
+    private SearchView filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital_select);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        this.rootBD = ConfigFireBase.getDataReference();
         this.medicEmail = (String)getIntent().getSerializableExtra("medicEmail");
         this.hospList = (ListView) findViewById(R.id.listHosp);
-        this.rootDB = ConfigFireBase.getDataReference();
-        final DatabaseReference auxRef = rootDB.child("Colaborador").child(this.medicEmail).child("hospitals");
-        auxRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        this.filter = (SearchView) findViewById(R.id.searchHospital);
 
-                String _auxS = dataSnapshot.getValue(String.class);
-                String[] _hosps = _auxS.split(" ");
-                hospList.setAdapter(new ArrayAdapter<String>(hospital_select.this, R.layout.pacient_item_list, _hosps));
+        //capture the size of the devices screen
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
 
-            }
+        //define the Layout height
+        RelativeLayout layout = (RelativeLayout) this.findViewById(R.id.bottonColor);
+        layout.setMinimumHeight(size.y/2);
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+        getHospitals();
+        setupSearchView();
 
-            }
-        });
         this.hospList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -57,5 +67,44 @@ public class hospital_select extends Activity {
         });
     }
 
+    private void setupSearchView(){
+        filter.setIconifiedByDefault(false);
+        filter.setOnQueryTextListener(this);
+        filter.setSubmitButtonEnabled(true);
+        filter.setQueryHint("Procure Hospital Aqui");
+    }
 
+    private void getHospitals(){
+        final DatabaseReference auxRef  = this.rootBD.child("Colaborador").child(this.medicEmail).child("hospitals");
+        auxRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String _auxS = dataSnapshot.getValue(String.class);
+                String[] _hosps = _auxS.split(" ");
+                hospList.setAdapter(new ArrayAdapter<String>(hospital_select.this, R.layout.pacient_item_list, _hosps));
+                hospList.setTextFilterEnabled(true);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        if (TextUtils.isEmpty(s)) {
+            hospList.clearTextFilter();
+        } else {
+            hospList.setFilterText(s.toString());
+        }
+        return true;
+    }
 }
